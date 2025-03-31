7da8b0805ea2214f3e0b18af2cd4a13250937714
lwmacct <lwmacct@icloud.com>
2025-03-31 14:21:38 +0800

    -

---
+++ 28.0.4-dind-2503310/apps/file/data/entry/init.d/88-docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+__main() {
+    _file="/data/entry/supervisor.d/docker.conf"
+    mkdir -p ${_file%/*}
+    cat >$_file <<EOF
+[program:docker]
+command=/usr/local/bin/dockerd-entrypoint.sh --exec-opt native.cgroupdriver=cgroupfs --storage-driver=overlay2 --dns=119.29.29.29 --dns=114.114.114.114 --log-driver=json-file --log-opt max-size=1m --log-opt max-file=1
+priority=999
+autostart=true
+autorestart=true
+stdout_logfile=/data/entry/logs/docker_out.log
+stderr_logfile=/data/entry/logs/docker_err.log
+EOF
+
+}
+
+__main
